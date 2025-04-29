e08a2549dab413b9b0c77565f686ba12e080d8c1
Mathguy23 <165744246+Mathguy23@users.noreply.github.com>
2025-04-29 16:10:17 -0400

    version

--- Hit.lua
+++ Hit.lua
@@ -4,7 +4,7 @@
 --- PREFIX: hit
 --- MOD_AUTHOR: [mathguy]
 --- MOD_DESCRIPTION: Blackjack instead of Poker
---- VERSION: 1.0.3
+--- VERSION: 1.0.4
 ----------------------------------------------
 ------------MOD CODE -------------------------
 -------------Credits--------------------------
